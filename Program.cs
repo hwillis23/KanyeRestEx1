d12c929b37ce23012ab35c1e0a239a9562eb501e
Hannah Willis <hwillis8817@gmail.com>
2022-04-19 22:43:35 -0500

    inital commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeRest
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //var kanyeURL = "https://api.kanye.rest/";
+
+            //var client = new HttpClient();
+
+            //var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            //var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            //Console.WriteLine(kanyeQuote);
+            //Console.WriteLine();
+
+
+            for (int i = 0; i < 5 ; i++)
+
+            {
+                QuoteMaker.KanyeQuote();      //class.method
+                QuoteMaker.RonQuote();
+
+            }
+
+           
+                  
+
+
+            
+
+
+
+        }
+
+
+
+    }
+}
---
+++ QuoteMaker.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeRest
+{
+    public class QuoteMaker
+    {
+        public static void KanyeQuote()
+        {
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var client = new HttpClient();
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye said: {kanyeQuote}");
+            Console.WriteLine($"------------------------");
+            Console.WriteLine();
+        }
+
+
+        public static void RonQuote()
+        {
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var client = new HttpClient();
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"__________________________________");
+
+            Console.WriteLine($"Ron said:{ronQuote}");
+
+               Console.WriteLine();
+
+       }
+              
+    }
+
+}
